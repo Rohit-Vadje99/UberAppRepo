45dd403aa8ed988d023c7a1e29fabf9108d6094b
Rohit Vadje-LocalLaptop <rohitvadje1999@gmai.com>
2026-02-24 17:10:03 +0530

    initial commit packages

---
+++ src/test/java/com/example/uberappproject/uberapp/UberappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.uberappproject.uberapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
